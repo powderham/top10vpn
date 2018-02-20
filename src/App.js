9859d6dd0d2f5ca8ec7ec6e8080af10974de2ab9
Edward Powderham <edpowderham@outlook.com>
2018-02-20 22:07:26 +0000

    implementing skeleton structure/first scss for layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div />
+        <div className="navbar">
+          <div>Top10VPN</div>
+          <div>|||</div>
+        </div>
+        <div className="user-input">
+          <div className="current-location">
+            <div className="input-question">
+              <div>Where are you now?</div>
+            </div>
+            <div className="input-select">
+              <div>Select box</div>
+            </div>
+          </div>
+          <div className="vpn-location">
+            <div className="input-question">
+              <div>Where do you want to VPN into?</div>
+            </div>
+            <div className="input-select">
+              <div>Select box</div>
+            </div>
+          </div>
+          <div className="text-period">
+            <div className="input-question">
+              <div>Period to test</div>
+            </div>
+            <div className="input-multi">Multi choice</div>
+          </div>
+        </div>
       </div>
     );
   }
